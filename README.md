b303932b75a5f1959bf90e6a6d516da0b6999445
Solomon Owerre <alaowerre@gmail.com>
2021-08-24 11:53:20 -0400

    added codes

--- README.md
+++ README.md
@@ -1,3 +1,33 @@
-# Statistical Analysis of Experimentation Data
+# Project Overview
 
-This repository will host some statistical analysis of A/B testing data.
+An e-commerce company conducted an A/B testing experiment to test if a new feature they recently introduced in their website through mobile app will increase revenue. They have measured the average revenue-per-user (ARPU) as the outcome (sucess metric) for each customer. The experiment was conducted by randomly assigning customers to the treatement with the new feature or the control without the new feature, at equal traffic. I have assumed that power analysis was conducted prior to performing this experiment.
+
+In this project, I have implemented different statistical tests on the data in order to validate if the new feature has a positive impact on the revenue. This is done by establishing a statistically significant result.  For pedagogical purposes, I have implemented the following two sample statistical tests:
+
+1). Student's t-test
+
+2). Welch's t-test
+
+3). Mann-Whitney u-test
+
+
+# Data Exploration
+Below are some figures from exploratory data anaysis.
+
+## 1. Treatment and Control Groups
+The distribution of ARPU is highly right skewed as expected, since some customers spent more than others. Therefore, I have transformed it by log base 10. The mean of ARPU in the treatment group is higher than the control group. I need to establish if the difference is statistically significant. 
+
+![fig](images/fig1.png)
+
+## 2. Segmentation by Phone Operating Systems
+The distributions below show segmentation by phone operating systems within the treatment and control groups. The results show disproportionality in the number of customers and total ARPU across the phone operating systems.
+
+In practice, any interesting feature in the data should be investigated by invoking the Twyman's law. For instance, one might want to investigate if the new feature is compactible with different phone operating systems.
+
+![fig](images/fig4.png)
+
+![fig](images/fig3.png)
+
+![fig](images/fig2.png)
+
+
---
+++ src/test.py
@@ -0,0 +1,78 @@
+####################################
+# Author: S. A. Owerre
+# Date modified: 24/07/2021
+# Class: Statistical Tests
+####################################
+
+# filter warnings
+import warnings
+warnings.filterwarnings("ignore")
+
+# stat analysis
+from scipy import stats
+
+class StatisticalTest:
+    """
+    Class for conducting parametric and non-parametric statistical tests
+    """
+    def __init__(self):
+        """
+        Define default parameters
+        """
+    
+    def student_ttest(self, treat_sample, contr_sample):
+        """
+        Compute the test statistic and the p-value 
+        for a two-sample Student's t-test
+
+        Parameter
+        ---------
+        treat_sample: pandas series of treatment sample
+        contr_sample: pandas series of control sample
+
+        Return
+        ------
+        test statistic & p-value
+        """
+        x = treat_sample.tolist()
+        y = contr_sample.tolist()
+        t, pval = stats.ttest_ind(x, y, equal_var=True, alternative='two-sided')
+        return t,pval
+
+    def welch_ttest(self, treat_sample, contr_sample):
+        """
+        Compute the test statistic and the p-value 
+        for a two-sample Welch's t-test
+
+        Parameter
+        ---------
+        treat_sample: pandas series of treatment sample
+        contr_sample: pandas series of control sample
+
+        Return
+        ------
+        test statistic & p-value
+        """
+        x = treat_sample.tolist()
+        y = contr_sample.tolist()
+        t, pval = stats.ttest_ind(x, y, equal_var=False, alternative='two-sided')
+        return t,pval
+        
+    def mann_whitney_utest(self, treat_sample, contr_sample):
+        """
+        Compute the U statistic and the p-value 
+        for a two-sample Mann-Whitney U test
+
+        Parameter
+        ---------
+        treat_sample: pandas series of treatment sample
+        contr_sample: pandas series of control sample
+
+        Return
+        ------
+        U statistic & p-value
+        """
+        x = treat_sample.tolist()
+        y = contr_sample.tolist()
+        u, pval = stats.mannwhitneyu(x, y, use_continuity=True)
+        return u,pval
